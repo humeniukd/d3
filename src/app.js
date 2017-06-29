b1387af23a99ee146ba566b34ce9da623e4e12ea
Dmytro Humeniuk <humeniukd_epam@ryanair.com>
2017-06-29 12:27:10 +0200

    start

--- src/app.js
+++ src/app.js
@@ -3,17 +3,12 @@ import prepare from './audio'
 import Worker from './worker'
 import Player from './player'
 import freq from './frequency'
-import { select, mouse } from 'd3-selection'
-import { scaleLinear, scaleTime } from 'd3-scale'
-import { timeFormat } from 'd3-time-format'
-import { axisLeft, axisRight, axisBottom } from 'd3-axis'
-import { area } from 'd3-shape'
 
 const height = 200
 const width = 800
 const worker = Worker()
 
-let player, g, gradient, flag, timeDisplay, label, x
+let player, flag
 
 const fx = () => {
   player.disconnect()
@@ -23,95 +18,16 @@ const fx = () => {
 
 document.querySelector('button').addEventListener('click', fx)
 
-const wf = select('#wf')
-
-const y = scaleLinear()
-  .domain([0., 1.])
-  .range([height, 0])
-
-const Y = scaleLinear()
-  .domain([-120, 0])
-  .range([height, 0])
-
-const YAxis = axisLeft(Y).ticks(5).tickSize(5)
-const yAxis = axisRight(y).ticks(10).tickPadding(10).tickSize(0)
-
-const a = area()
-  .x((d, i) => i)
-  .y0(height)
-  .y1(d => y(d))
-
-const A = area()
-  .x((d, i) => i)
-  .y0(height)
-  .y1(d => {
-    let db = -120
-    if (d > 0)
-      db = Math.log(d) / Math.LN10 * 20
-    return Y(db)
-  })
-
 const init = (buffer) => {
-  wf.html('')
   player && player.stop()
   player = new Player(buffer)
-
-  x = scaleTime()
-    .domain([0, player.duration*1000])
-    .range([0, width])
-
-  timeDisplay = x.tickFormat(16, timeFormat('%M:%S'))
-
-  const xAxis = axisBottom(x).ticks(16).tickFormat(timeDisplay)
-
-  const defs = wf.append('defs')
-  gradient = defs.append('linearGradient').attr('id', 'progress')
-    .attr('x1', '0%')
-    .attr('y1', '0%')
-    .attr('x2', '100%')
-    .attr('y2', '0%')
-
-  gradient.append('stop')
-    .attr('stop-color', 'magenta')
-    .attr('offset', '0%')
-
-  gradient.append('stop')
-    .attr('stop-color', 'blueviolet')
-    .attr('offset', '0%')
-
-  g = wf.append('g')
-
-  g.append('g')
-    .attr('class', 'axis y left')
-    .call(YAxis)
-  g.append('g')
-    .attr('class', 'axis y right')
-    .attr('transform', `translate(${width},0)`)
-    .call(yAxis)
-  g.append('g')
-    .attr('class', 'axis x')
-    .attr('transform', `translate(0,${height})`)
-    .call(xAxis)
-  label = g.append('g')
-    .append('text')
-    .text('0:00')
-    .attr('class', 'time')
-    .attr('transform', `translate(${width-50},10)`)
 }
 
 const progress = () => {
   requestAnimationFrame(progress)
-  const pos = player.currentTime / player.duration * 100
-  label.text(timeDisplay(player.currentTime*1000))
-  gradient.attr('x1', `${pos}%`)
+  console.log(player.currentTime)
 }
 
-wf.on('click', function() {
-  const m = mouse(this)
-  const x = m[0]
-  const k = x/width
-  player && player.play(k)
-})
 const input = document.querySelector('input')
 
 document.addEventListener('keyup', (e) => {
@@ -128,8 +44,7 @@ input.addEventListener('change', e => prepare(e.target.files[0]).then(b => {
   init(b)
   worker.postMessage(b.getChannelData(0))
   worker.onmessage = ({data}) => {
-    g.append('path').attr('d', A(data)).attr('fill', 'url(#progress)')
-    g.append('path').attr('d', a(data)).attr('fill', 'cyan')
+    console.log(data)
     player.play()
     progress()
     freq()
--- src/frequency.js
+++ src/frequency.js
@@ -1,6 +1,3 @@
-import { select } from 'd3-selection'
-import { scaleLinear, scaleLog } from 'd3-scale'
-import { axisLeft, axisBottom } from 'd3-axis'
 import { ac } from './audio'
 
 export const analyser = ac.createAnalyser()
@@ -10,65 +7,12 @@ analyser.fftSize = fftSize
 analyser.minDecibels = -90
 analyser.maxDecibels = -10
 
-const height = 100
-const width = 400
-
-const svg = select('#eq').attr('height', height).attr('width', width)
-
 const frequencyData = new Uint8Array(analyser.frequencyBinCount)
-
-svg.selectAll('rect')
-  .data(frequencyData)
-  .enter()
-  .append('rect')
-  .attr('x', (d, i) => i * width / frequencyData.length)
-  .attr('width', width / frequencyData.length - 1)
-
-const chartGroup = svg.append('g')
-
-const y = scaleLinear()
-  .domain([255, 0])
-  .range([height, 0])
-
-const y1 = scaleLinear()
-  .domain([0, -60])
-  .range([0, height]);
-
-const x = scaleLinear()
-  .domain([4, frequencyData.length-1])
-  .range([0, width])
-
-const yAxis = axisLeft(y1).ticks(3).tickSize(5)
-const xAxis = axisBottom(x).ticks(frequencyData.length-1)
-  .tickSize(width / frequencyData.length - 1)
-  .tickSize(5)
-  .tickFormat(d => {
-    const val = Math.round(Math.pow(10, (d * 2 + 12)/10))
-    return val > 1e3 ? `${Math.ceil(val/1e3)}k` : val
-  })
-const color = scaleLinear().domain([0, 127, 255]).range(['lawngreen ', 'gold', 'orangered'])
-
-chartGroup.append('g')
-  .attr('class', 'freq y')
-  .call(yAxis)
-
-chartGroup.append('g')
-  .attr('class', 'freq x')
-  .attr('transform', `translate(0, ${height})`)
-  .call(xAxis)
 
 const render = () => {
   requestAnimationFrame(render)
-
-  // Copy frequency data to frequencyData array.
   analyser.getByteFrequencyData(frequencyData)
-
-  // Update d3 chart with new data.
-  svg.selectAll('rect')
-    .data(frequencyData)
-    .attr('y', d => height - y(d))
-    .attr('height', d => y(d))
-    .attr('fill', color) //`rgb(255, ${255-d}, 0)`
+  console.log(frequencyData)
 }
 
 export default render
